4cfae64f9a3b0841c86295ff1ca208e2a380d110
huangzhh2001-ux <huangzhh2001@gmail.com>
2026-06-07 13:16:08 +0800

    Create deploy.R

---
+++ deploy.R
@@ -0,0 +1,6 @@
+rsconnect::setAccountInfo(
+  name = "25085502um",
+  token = "56DD119AAC8CE54F26B5C35DF9E0FA7D",
+  secret = "3SA7StNusB+6jR3tXMGBKYW3VJ7PDNcRhvXwAG3a"
+)
+rsconnect::deployApp(appDir = ".", appName = "aging-dashboard", forceUpdate = TRUE)
